4c23b802cb03c9db6e3be0e4db221a2bb7ee5072
Admin <admin@mail.ru>
2021-02-05 14:27:29 +0300

    3. Арифметические операции. [#452379]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -3,5 +3,29 @@ package ru.job4j;
 public class Main {
     public static void main(String [] args) {
         System.out.println("Hello world");
+
+        // возраст
+        byte age = 127;
+
+        // пол
+        char sex = 'M';
+
+        // рост
+        short growth = 190;
+
+        // количество кирпичей для постройки дома
+        short amountBricks = 32767;
+
+        // количество бактерий
+        int amountBacterium = 40000000;
+
+        // расстояние до космических объектов
+        long distanceToSun = 150000000;
+
+        // разрешение на работу
+        boolean readyToWork = true;
+
+        // размер файла.
+        int fileSize = 8000000;
     }
 }
