ef2760334c7c48882bdf930103cb4f5d0e3dc05d
Admin <admin@mail.ru>
2021-02-15 09:31:11 +0300

    5.4. Протеиновая диета [#452423]

--- src/ru/job4j/CheckPrimeNumber.java
+++ src/ru/job4j/CheckPrimeNumber.java
@@ -2,13 +2,19 @@ package ru.job4j;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        for (int i = 2; i > 0; i--) {
 
-            if (number % i == 0 && number != i && i != 1 || number == 1) {
-                prime = false;
+        boolean prime = number > 1;
+
+        for (int i = 2; i < number; i++) {
+
+            if (number % i == 0) {
                 break;
             }
+
+/*            if (number % i == 0 && number != i && i != 1 || number == 1) {
+                prime = false;
+                break;
+            }*/
         }
         return prime;
     }
