e7bb81a2e3c14fd965664714bda9f9b0542a8935
Admin <admin@mail.ru>
2021-02-16 13:42:22 +0300

    6.4.2. Слово заканчивается на ... [#452339]

---
+++ test/ru/job4j/EndsWithTest.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EndsWithTest {
+
+    @Test
+    public void whenEndWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        Assert.assertEquals(result, true);
+        //assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotEndWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] post = {'l', 'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        Assert.assertEquals(result, false);
+        //assertThat(result, is(false));
+    }
+}
